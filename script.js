9205096538c44341c603788c05d2b890fd7ee99a
Tiffany Fust <tiffanyfust@gmail.com>
2021-09-12 15:49:37 -0700

    added firestore database and trailer details page

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+const dbRef = db.collection('trailers');
+
+console.log(dbRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data().sleeps);
+    });
+}))
+
+const trailers = document.querySelectorAll('.trailer');
+
+
+trailers.forEach((trailer) => {
+    trailer.addEventListener('click', () => {
+        const trailerId = trailer.id;
+        console.log(trailerId)
+    })
+})
