a00a61e49727f9da891ba9239cb9a4b14d4ac824
Tiffany Fust <tiffanyfust@gmail.com>
2021-09-22 16:37:41 -0700

    created user controlled image carousel

--- script.js
+++ script.js
@@ -64,67 +64,134 @@ const displayData = (trailerId) => {
 }
 displayData('trailer1')
 
-
+const trailerImages = [];
 // Now we get the references of these images
 imagesRef.listAll().then(function(result) {
     
     result.items.forEach(function(imageRef) {
-      // And finally display them
-    displayImage(imageRef);
-    });
+        // And finally display them
+        imageRef.getDownloadURL().then(function(url) {
+            trailerImages.push(url)
+            
+            if (trailerImages.length == result.items.length) {
+                displayImages(trailerImages)
+            }
+        })
+    })
+    
 }).catch(function(error) {
     // Handle any errors
 })
 
 
-function displayImage(imageRef) {
-    imageRef.getDownloadURL().then(function(url) {
-        const test = url;
+function displayImages(imagesArr) {
 
+    const index = imagesArr.findIndex((url) => {
+        return url.indexOf('main.jpg')!==-1;
+    })
+
+    const mainUrl = imagesArr[index];
+    imagesArr.splice(index, 1);
+    imagesArr.unshift(mainUrl);
+
+    for (i = 0; i < imagesArr.length; i++) {
         const imgContainer = document.querySelector('.trailerImages');
         const newLi = document.createElement('li');
         const img = document.createElement('img');
-        img.src = test;
+        img.src = imagesArr[i];
 
-        // adds a mainImage class to the main Image
-        if (imageRef._delegate._location.path_ === 'hideout/main.jpg') {
-            newLi.classList.add('mainImage')
+        newLi.classList.add('image');
+
+        if (i === 0) {
+            newLi.classList.add('mainImage');
         } else {
             newLi.classList.add('smallImage')
         }
 
         newLi.append(img);
         imgContainer.append(newLi);
-    }).then (function() {
-        const smallImages = document.querySelectorAll('.smallImage');
-        const main = document.querySelector('.mainImage');
-        main.addEventListener('click', () => changeMain(main));
-        smallImages.forEach((image) => {
-            image.addEventListener('click', () => changeMain(image))
-        })
-    }).catch(function(error) {
-      // Handle any errors
-    });
+    } 
 }
 
-const changeMain = (image) => {
-    const mainImage = document.querySelector('.mainImage')
-    mainImage.classList.add('smallImage');
-    mainImage.classList.remove('mainImage');
-    image.classList.add('mainImage');
-    image.classList.remove('smallImage');
+const changeMain = (event) => {
+        const images = document.querySelectorAll('.image');
+        const arr = [...images];
+    const index = arr.findIndex((item) => {
+        return item.classList.contains('mainImage');
+    })
+
+    if (event.target.classList.contains('fa-arrow-right')) {
+        arr[index].classList.remove('mainImage');
+        arr[index].classList.add('smallImage');
+        if (index >= arr.length - 1) {
+            arr[index - (arr.length - 1)].classList.remove('smallImage');
+            arr[index - (arr.length - 1)].classList.add('mainImage');
+        } else {
+            arr[index + 1].classList.add('mainImage');
+            arr[index + 1].classList.remove('smallImage');
+        }
+        
+    } else {
+        arr[index].classList.remove('mainImage');
+        arr[index].classList.add('smallImage');
+
+        if (index === 0) {
+            arr[index + (arr.length - 1)].classList.add('mainImage');
+            arr[index + (arr.length - 1)].classList.remove('smallImage');
+        } else {
+            arr[index - 1].classList.remove('smallImage');
+            arr[index - 1].classList.add('mainImage');
+        }
+    }
+
 }
 
+const controls = document.querySelectorAll('.control');
+
+controls.forEach((control) => {
+    control.addEventListener('click', (e) => changeMain(e))
+})
 
 
-// storageRef.child('hideout/main.jpg').getDownloadURL().then(function(url) {
-//     const test = url;
-//     document.querySelector('.imgTest').src = test;
+// const changeMain = (image) => {
+//     const mainImage = document.querySelector('.mainImage')
+//     mainImage.classList.add('smallImage');
+//     mainImage.classList.remove('mainImage');
+//     image.classList.add('mainImage');
+//     image.classList.remove('smallImage');
+// }
 
-// }).catch(function(error) {
 
-// });
 
-// const test = 'firebase_url';
+// function displayImages(imageRef) {
+//     imageRef.getDownloadURL().then(function(url) {
+//         const test = url;
 
-// document.querySelector('.imgTest').src = test;
+//         const imgContainer = document.querySelector('.trailerImages');
+//         const newLi = document.createElement('li');
+//         const img = document.createElement('img');
+//         img.src = test;
+
+//         // adds a mainImage class to the main Image
+//         if (imageRef._delegate._location.path_ === 'hideout/main.jpg') {
+//             newLi.classList.add('mainImage')
+//         } else {
+//             newLi.classList.add('smallImage')
+//         }
+
+//         newLi.append(img);
+//         imgContainer.append(newLi);
+//     }).then (function() {
+//         const smallImages = document.querySelectorAll('.smallImage');
+//         const main = document.querySelector('.mainImage');
+//         main.addEventListener('click', () => changeMain(main));
+//         smallImages.forEach((image) => {
+//             image.addEventListener('click', () => changeMain(image))
+//         })
+//     }).catch(function(error) {
+//       // Handle any errors
+//     });
+// }
+
+
+
